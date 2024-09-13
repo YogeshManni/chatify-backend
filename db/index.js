1b31a0f657f791084fb78cdfc23cb48940c6751e
Yogesh Manni <yogeshmanni786@gmail.com>
2024-09-12 21:47:49 -0700

    remove ssl authorization while connecting with db

--- db/index.js
+++ db/index.js
@@ -11,9 +11,9 @@ module.exports = class appDb {
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
       host: process.env.DB_HOST,
-      /* ssl: {
-        rejectUnauthorized: false
-      } */
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
     this.db.connect((err) => {
       if (!err) {
